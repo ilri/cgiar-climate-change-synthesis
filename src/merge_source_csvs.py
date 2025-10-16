ed419a29b29cb78c3e7262054829372cdb69c136
Alan Orth <alan.orth@gmail.com>
2025-10-16 13:34:11 +0300

    src/merge_source_csvs.py: more IRRI author fixes
    Whitespace.

--- src/merge_source_csvs.py
+++ src/merge_source_csvs.py
@@ -257,8 +257,14 @@ df_irri["Authors"] = df_irri["Authors"].str.replace(
     r"((;\s)?MISSING DATA(;\s)?)", "", regex=True
 )
 
+# Remove spaces before semicolons
+df_irri["Authors"] = df_irri["Authors"].str.replace(r"\s+;", ";", regex=True)
+
 # Add spaces after semicolons where they are missing
 df_irri["Authors"] = df_irri["Authors"].str.replace(r";([^ ])", r"; \1", regex=True)
+
+# Strip trailing spaces
+df_irri["Authors"] = df_irri["Authors"].str.strip()
 
 # Replace "backcrossing.;climatic change." → "backcrossing; climatic change"
 df_irri["subjects"] = df_irri["subjects"].str.replace(r"\.?;", "; ", regex=True)
